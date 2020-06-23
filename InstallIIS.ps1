74581bf3834cbe326e60f5065281a15dae0892b0
CortexProjectsDepartment <43964841+CortexProjectsDepartment@users.noreply.github.com>
2020-06-23 09:38:44 +0100

    Add files via upload

---
+++ InstallIIS.ps1
@@ -0,0 +1,15 @@
+Install-WindowsFeature -Name Web-Static-Content
+Install-WindowsFeature -Name Web-Default-Doc
+Install-WindowsFeature -Name Web-Dir-Browsing
+Install-WindowsFeature -Name Web-Http-Errors
+Install-WindowsFeature -Name Web-Asp-Net45
+Install-WindowsFeature -Name Web-Net-Ext45
+Install-WindowsFeature -Name Web-ISAPI-Ext
+Install-WindowsFeature -Name Web-ISAPI-Filter
+Install-WindowsFeature -Name Web-Http-Logging
+Install-WindowsFeature -Name Web-Request-Monitor
+Install-WindowsFeature -Name Web-Windows-Auth
+Install-WindowsFeature -Name Web-Filtering
+Install-WindowsFeature -Name Web-Stat-Compression
+Install-WindowsFeature -Name Web-Mgmt-Console
+Install-WindowsFeature -Name Web-Websockets

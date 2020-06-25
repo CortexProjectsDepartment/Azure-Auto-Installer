479a0c472de689ef71fbf257bbf485d43553c5c8
CortexProjectsDepartment <43964841+CortexProjectsDepartment@users.noreply.github.com>
2020-06-25 23:51:12 +0100

    Add files via upload

--- InstallIIS.ps1
+++ InstallIIS.ps1
@@ -15,3 +15,7 @@ Install-WindowsFeature -Name Web-Filtering
 Install-WindowsFeature -Name Web-Stat-Compression
 Install-WindowsFeature -Name Web-Mgmt-Console
 Install-WindowsFeature -Name Web-Websockets
+Install-WindowsFeature -Name Web-Http-Redirect
+Install-WindowsFeature -Name Web-Dyn-Compression
+Install-WindowsFeature -Name Web-Http-Errors
+Install-WindowsFeature -Name Web-Mgmt-Service
